4f8be2050d0bbb1bda926e3afa60da1cb3b01949
ayhan5834 <75215476+ayhan5834@users.noreply.github.com>
2026-06-01 03:03:42 +0300

    Update Denemeborsa.py

--- Denemeborsa.py
+++ Denemeborsa.py
@@ -364,7 +364,7 @@ with aktif_sekme[0]:
 
                         st.divider()
 
-                        col_sil, col_analiz = st.columns([1,1])
+                        col_sil, bosluk, col_analiz = st.columns([45,10,45])
 
                         with col_sil:
                             sil = st.button("🗑️ Sil", key=f"detay_sil_{h}", use_container_width=True )
